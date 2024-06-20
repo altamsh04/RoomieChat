1028e49572a993a6fd8a613ded5ed9e593774b78
altamsh <bairagdaraltamsh@gmail.com>
2024-06-21 02:29:12 +0530

    Added authentication

--- app/src/main/java/com/example/roomiechat/MainActivity.java
+++ app/src/main/java/com/example/roomiechat/MainActivity.java
@@ -1,16 +1,118 @@
 package com.example.roomiechat;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextEmail, editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewCreateAccount;
+
+    private FirebaseFirestore db;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, OptionsActivity.class));
+
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.loginIntoApplication);
+        textViewCreateAccount = findViewById(R.id.createAccount);
+
+        checkIfLoggedIn();
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+        textViewCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void checkIfLoggedIn() {
+        SharedPreferences sharedPreferences = getSharedPreferences("RoomieChatPrefs", MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
+
+        if (isLoggedIn) {
+            navigateToOptionsActivity();
+        }
+    }
+
+    private void loginUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            if (!password.isEmpty()) {
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                saveLoginState();
+                                startActivity(new Intent(MainActivity.this, OptionsActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("LOGIN_ERROR",e.getMessage());
+                                editTextEmail.setText("");
+                                editTextPassword.setText("");
+                            }
+                        });
+            } else {
+                editTextPassword.setError("Password cannot be empty");
+            }
+        } else if (email.isEmpty()) {
+            editTextEmail.setError("Email cannot be empty");
+        } else {
+            editTextEmail.setError("Please enter a valid email");
+        }
+    }
+
+    private void saveLoginState() {
+        SharedPreferences sharedPreferences = getSharedPreferences("RoomieChatPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("isLoggedIn", true);
+        editor.apply();
+    }
+
+    private void navigateToOptionsActivity() {
+        Intent intent = new Intent(MainActivity.this, OptionsActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/roomiechat/SignUpActivity.java
@@ -0,0 +1,130 @@
+package com.example.roomiechat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText editTextUsername, editTextEmail, editTextPassword;
+    private Button buttonSignUp;
+    private TextView loginToAccount;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        loginToAccount = findViewById(R.id.loginToAccount);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount();
+            }
+        });
+
+        loginToAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private void createAccount() {
+        String username = editTextUsername.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(username)) {
+            editTextUsername.setError("Username");
+            return;
+        }
+
+        if (TextUtils.isEmpty(email)) {
+            editTextUsername.setError("Email");
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            editTextUsername.setError("Password");
+            return;
+        }
+
+        checkUsernameExists(username, email, password);
+    }
+
+    private void checkUsernameExists(final String username, final String email, final String password) {
+        db.collection("users").document(username).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    Toast.makeText(SignUpActivity.this, "Username already exists", Toast.LENGTH_SHORT).show();
+                } else {
+                    createUser(email, password, username);
+                }
+            } else {
+                Log.d("AUTH_ALREADY", task.getException().getMessage());
+            }
+        });
+    }
+
+    private void createUser(String email, String password, String username) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        if (user != null) {
+                            saveUserToFirestore(user.getUid(), username, email);
+                        }
+                    } else {
+                        Log.d("AUTH_ERROR", task.getException().getMessage());
+                    }
+                });
+    }
+
+    private void saveUserToFirestore(String userId, String username, String email) {
+        Map<String, Object> user = new HashMap<>();
+        user.put("username", username);
+        user.put("email", email);
+
+        db.collection("users").document(username)
+                .set(user)
+                .addOnSuccessListener(aVoid -> {
+                    Log.d("SIGNUP_SUCCESS","Account created successfully");
+                    startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    Log.d("SIGNUP_ERROR", e.getMessage());
+                });
+    }
+}
